ddc640196fd1fb237c8a40b19dae04470a3a2595
Prabhat Maurya <prabhat@e.email>
2024-01-21 14:32:27 +0530

    initial site

---
+++ scripts/home.js
@@ -0,0 +1,15 @@
+function download(){
+    window.open("https://sourceforge.net/projects/project-kizashi/files/",'_blank').focus();
+}
+function source()
+{
+    window.open("https://github.com/Project-Kizashi",'_blank').focus();
+}
+function donate()
+{
+    window.open("https://github.com/PrabhatProxy",'_blank').focus();
+}
+function telegram()
+{
+    window.open("https://t.me/ProjectKizashi",'_blank').focus();
+}
