8d4e53201b2de6d2dabb62d8c05358cb7cdfbb97
Prabhat Maurya <prabhat@e.email>
2024-03-12 20:11:18 +0530

    new site update

--- scripts/home.js
+++ scripts/home.js
@@ -13,3 +13,34 @@ function telegram()
 {
     window.open("https://t.me/ProjectKizashi",'_blank').focus();
 }
+
+function my_telegram()
+{
+    window.open("https://t.me/prabhat_proxy",'_blank').focus();
+}
+function insta()
+{
+    window.open("https://www.instagram.com/prabhat.maurya_/",'_blank').focus();
+}
+function linkedin()
+{
+    window.open("https://www.linkedin.com/in/prabhat-kumar-maurya-994816249/",'_blank').focus();
+}
+
+function donate_rahul()
+{
+    window.open("https://github.com/Noctowl709",'_blank').focus();
+}
+
+function my_telegram_rahul()
+{
+    window.open("https://t.me/Nekotalia",'_blank').focus();
+}
+function insta_rahul()
+{
+    window.open("https://www.instagram.com/noctowl709/",'_blank').focus();
+}
+function linkedin_rahul()
+{
+    window.open("https://www.linkedin.com/in/rahul-talukdar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",'_blank').focus();
+}
