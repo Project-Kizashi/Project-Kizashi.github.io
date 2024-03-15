bf4e38c65a1dc90833ce6c0634f3a71ab7d14eee
Prabhat Maurya <prabhat@e.email>
2024-03-15 19:10:44 +0530

    version 2.1

--- scripts/home.js
+++ scripts/home.js
@@ -1,19 +1,7 @@
-function setupClickHandlers() {
-    function openlink(link) {
-        if (link == "devices.html") {
-            window.open(link, '_self').focus();
-        } else {
-            window.open(link, '_blank').focus();
-        }
+function openlink(link) {
+    if (link == "devices.html") {
+        window.open(link, '_self').focus();
+    } else {
+        window.open(link, '_blank').focus();
     }
-
-    let clickable = document.querySelectorAll(".clickable");
-
-    clickable.forEach(function (clickable) {
-        clickable.addEventListener("click", function (eve) {
-            openlink(eve.target.id);
-        });
-    });
 }
-
-document.addEventListener('DOMContentLoaded', setupClickHandlers);
